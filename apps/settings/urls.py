6d40b5539dae09ac217738c1db5d4917b73010f7
Beksultan06 <nurlanuuulubeksultan@gmail.com>
2025-08-23 17:43:19 +0600

    end html pages about

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -1,11 +1,29 @@
 from django.urls import path
-from apps.settings.views import LibraryAPIView, BookAPIView, BookCreateAPIView, BookRetrieveAPIVIEW, BookUdateAPIView, BookDeleteAPIView
+
+from rest_framework.routers import DefaultRouter
+from apps.settings.views import BookAPI, BookAPIList
+
+router = DefaultRouter()
+router.register('book-list', BookAPIList, basename='book-list')
+router.register('book', BookAPI, basename='book')
+
 
 urlpatterns = [
-    path("list-library", LibraryAPIView.as_view(), name='list-library'),
-    path("book-list", BookAPIView.as_view(), name='book-list'),
-    path("book-create/", BookCreateAPIView.as_view(), name='book-create'),
-    path("book-detail/<int:pk>/", BookRetrieveAPIVIEW.as_view(), name='detail-book'),
-    path("book-update/<int:pk>/", BookUdateAPIView.as_view(), name='book-update'),
-    path("book-delete/<int:pk>/", BookDeleteAPIView.as_view(), name='book-delete')
+    
 ]
+
+
+urlpatterns += router.urls
+
+
+
+# from apps.settings.views import LibraryAPIView, BookAPIView, BookCreateAPIView, BookRetrieveAPIVIEW, BookUdateAPIView, BookDeleteAPIView
+
+# urlpatterns = [
+#     path("list-library", LibraryAPIView.as_view(), name='list-library'),
+#     path("book-list", BookAPIView.as_view(), name='book-list'),
+#     path("book-create/", BookCreateAPIView.as_view(), name='book-create'),
+#     path("book-detail/<int:pk>/", BookRetrieveAPIVIEW.as_view(), name='detail-book'),
+#     path("book-update/<int:pk>/", BookUdateAPIView.as_view(), name='book-update'),
+#     path("book-delete/<int:pk>/", BookDeleteAPIView.as_view(), name='book-delete')
+# ]
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,40 +1,57 @@
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, \
+RetrieveAPIView, DestroyAPIView
 from rest_framework.response import Response
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, \
+RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.viewsets import GenericViewSet
 
 from apps.settings.models import Library, Book
-from apps.settings.serializers import LibrarySerilizer, BookSerializer, BookDetailSerializer
-
-
-class LibraryAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        library = Library.objects.all()
-        serializer = LibrarySerilizer(library, many=True)
-        return Response(serializer.data)    
-
+from apps.settings.serializers import LibrarySerilizer, BookSerializer,\
+ BookDetailSerializer
 
 @method_decorator(cache_page(60), name='dispatch')
-class BookAPIView(ListAPIView):
+class BookAPIList(GenericViewSet, ListModelMixin):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-
-class BookCreateAPIView(CreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-
-class BookRetrieveAPIVIEW(RetrieveAPIView):
+class BookAPI(GenericViewSet, 
+              CreateModelMixin,
+              RetrieveModelMixin,
+              UpdateModelMixin,
+              DestroyModelMixin):
     queryset = Book.objects.all()
     serializer_class = BookDetailSerializer
 
-
-class BookUdateAPIView(UpdateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class BookDeleteAPIView(DestroyAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+# class LibraryAPIView(APIView):
+#     def get(self, request, *args, **kwargs):
+#         library = Library.objects.all()
+#         serializer = LibrarySerilizer(library, many=True)
+#         return Response(serializer.data)    
+# 
+# 
+# @method_decorator(cache_page(60), name='dispatch')
+# class BookAPIView(ListAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+# 
+# 
+# class BookCreateAPIView(CreateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+# 
+# 
+# class BookRetrieveAPIVIEW(RetrieveAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookDetailSerializer
+# 
+# 
+# class BookUdateAPIView(UpdateAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+# 
+# class BookDeleteAPIView(DestroyAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
